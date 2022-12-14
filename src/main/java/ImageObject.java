1a04393cab86bd16b6dbc0dc6543365fa4456ad6
sarastavnitser <sstavnit@student.touro.edu>
2022-12-14 00:25:46 -0500

    methods to convert pixels to energy

---
+++ src/main/java/ImageObject.java
@@ -0,0 +1,69 @@
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+import static javax.imageio.ImageIO.read;
+
+public class ImageObject {
+    private BufferedImage image = null;
+    private Color[][] pixelGrid = null;
+
+    private int[][] energyGrid = null;
+    private int height;
+
+    private int width;
+
+    public ImageObject() {
+        try {
+            File imageFile = new File("src/Broadway_tower_edit.jpg");
+            image = ImageIO.read(imageFile);
+            generatePixelArray();
+            generateEnergyGrid();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void generatePixelArray() {
+        width = image.getWidth();
+        height = image.getHeight();
+        pixelGrid = new Color[height][width];
+        energyGrid = new int[height][width];
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                pixelGrid[row][col] = new Color(image.getRGB(col, row));
+            }
+        }
+
+    }
+
+    public void generateEnergyGrid() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || j == 0 || i == height - 1 || j == width - 1) {
+                    continue;
+                } else {
+                    int energy = (int) (Math.pow(pixelGrid[i - 1][j].getRed() - pixelGrid[i + 1][j].getRed(), 2) +
+                                                Math.pow(pixelGrid[i - 1][j].getGreen() - pixelGrid[i + 1][j].getGreen(), 2) +
+                                                Math.pow(pixelGrid[i - 1][j].getBlue() - pixelGrid[i + 1][j].getBlue(), 2) +
+                                                Math.pow(pixelGrid[i][j + 1].getRed() - pixelGrid[i][j - 1].getRed(), 2) +
+                                                Math.pow(pixelGrid[i][j + 1].getGreen() - pixelGrid[i][j - 1].getGreen(), 2) +
+                                                Math.pow(pixelGrid[i][j + 1].getBlue() - pixelGrid[i][j - 1].getBlue(), 2));
+
+                    energyGrid[i][j] = energy;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ImageObject m = new ImageObject();
+
+    }
+}
+
---
+++ src/main/java/Pixel.java
@@ -0,0 +1,46 @@
+public class Pixel {
+    private int red;
+    private int green;
+    private int blue;
+    private int alpha;
+
+    public Pixel(int red, int green, int blue, int alpha) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(int alpha) {
+        this.alpha = alpha;
+    }
+
+}
